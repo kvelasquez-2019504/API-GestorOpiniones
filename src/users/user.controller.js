efbe76c17d5f441238bfae95b642199efae0b2bc
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-29 11:56:05 -0600

    Se comenzo el uso de jswonwebtoken

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -2,11 +2,15 @@ import {request,response} from 'express';
 import bcryptjs from 'bcryptjs';
 import User from './user.model.js';
 
+export const userPut= async(req,res)=>{
+    
+}
+
 export const userPost = async (req ,res)=>{
     const {username,userEmail,password}=req.body;
     const user = new User({username,userEmail,password});
     const salt = bcryptjs.genSaltSync();
-    usuario.password = bcryptjs.hashSync(password, salt);
+    user.password = bcryptjs.hashSync(password, salt);
     await user.save();
     res.status(200).json({
         user
