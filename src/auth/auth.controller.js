c10ba4ae0969698932efe1ada0a84ba31bf5460a
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-29 22:09:28 -0600

    Se agrego validacion de token

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -10,24 +10,24 @@ export const login = async (req, res) => {
         });
         //verifico que el usuario existe
         if (!userLog) {
-            res.status(400).json({
-                msg: "Username or email are not valid"
-            });
-        } else{
-            //verifico que la clave del usuario sea igual
-            const verifyPassword = bcryptjs.compareSync(password, userLog.password);
-            if (!verifyPassword) {
-                res.status(400).json({
-                    msg: "Password is incorrect"
-                });
-            }
-            const token = await generateJWT(userLog.id);
-            res.status(200).json({
-                msg: "YOU ARE LOGGED IN THE APP",
-                userLog,
-                token
+            return res.status(400).json({
+                msg: "Credentials not valid"
             });
         }
+        //verifico que la clave del usuario sea igual
+        const verifyPassword = bcryptjs.compareSync(password, userLog.password);
+        if (!verifyPassword) {
+            return res.status(400).json({
+                msg: "Password is incorrect"
+            });
+        }
+        const token = await generateJWT(userLog.id);
+        res.status(200).json({
+            msg: "YOU ARE LOGGED IN THE APP",
+            userLog,
+            token
+        });
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
---
+++ src/middlewares/validate-jwt.js
@@ -0,0 +1,30 @@
+import jwt  from "jsonwebtoken";
+import User from "../users/user.model.js";
+
+export const validateJWT = async(req,res,next)=>{
+    const token = req.header("x-token");
+    if(!token){
+        return res.status(401).json({
+            msg:"There is no token in the request."
+        });
+    }
+    try {
+        //verificacion de token
+        const {uid}= jwt.verify(token,process.env.SECRETORPRIVATEKEY);
+        //leer el usuerio con el id de token
+        const user= await User.findById(uid);
+        //verificar que existe el usuario
+        if(!user){
+            return res.status(401).json({
+                msg:"The user does not exist in the database."
+            })
+        }
+        req.user=user;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            msg:"No valid token"
+        });
+    }
+}
