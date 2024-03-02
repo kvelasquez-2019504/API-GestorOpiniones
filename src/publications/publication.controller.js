ce0a87f90f9e744c2c2f4a942e70e223fc252809
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-03-01 22:32:37 -0600

    Se agrego edicion y ver publication

--- src/publications/publication.controller.js
+++ src/publications/publication.controller.js
@@ -1,17 +1,55 @@
-import { request,response } from "express";
+import { request, response } from "express";
 import Publication from '../publications/publication.model.js';
 import User from "../users/user.model.js";
 
-export const publicationPost= async (req=request,res=response)=>{
-    const {id} =req.user;
+export const publicationGet= async(req,res)=>{
+    const {id}=req.user;
+    const userLog = await User.findById(id);
+    let myPublications=[];
+    for(let idPublication of userLog.publications){
+        const publication = await Publication.findById(idPublication);
+        if(publication){
+            myPublications.push(publication);
+        }
+    }
+    res.status(200).json({
+        myPublications
+    });
+}
+
+export const verifyIdPublication = async (req, res, next) => {
+    const { idPublication } = req.params;
+    const { id } = req.user;
+    const userLog = await User.findById(id);
+    if (!userLog.publications.includes(idPublication)) {
+        return res.status(400).json({
+            msg: "The publication not is your"
+        });
+    }
+    next();
+}
+
+export const publicationPut = async (req = request, res = response) => {
+    const { idPublication } = req.params;
+    const { _id, idUser, ...publication } = req.body;
+    await Publication.findByIdAndUpdate(idPublication,{...publication});
+    const newPublication = await Publication.findById(idPublication);
+    res.status(200).json({
+        msg:"The publication is now updated",
+        newPublication
+    });
+}
+
+export const publicationPost = async (req = request, res = response) => {
+    const { id } = req.user;
     let userLog = await User.findById(id);
-    const {title, mainText,category}=req.body;
-    const publication = new Publication({title,mainText,category,idUser:id});
+    const { title, mainText, category } = req.body;
+    const publication = new Publication({ title, mainText, category, idUser: id });
     userLog.publications.push(publication.id);
-    await User.findByIdAndUpdate(id,{publications:userLog.publications});
+    await User.findByIdAndUpdate(id, { publications: userLog.publications });
     publication.save();
     res.status(200).json({
-        msg:"The publication is upload.",
+        msg: "The publication is upload.",
         publication
     })
 }
--- src/publications/publication.routes.js
+++ src/publications/publication.routes.js
@@ -1,11 +1,28 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { publicationPost } from "./publication.controller.js";
+import { publicationGet,
+    publicationPost,
+    verifyIdPublication,
+    publicationPut } from "./publication.controller.js";
 import {verifyLengthCategory}  from '../helpers/db-validator.js';
 import { validateFields } from "../middlewares/validate-field.js";
 import { validateJWT } from "../middlewares/validate-jwt.js";
 
 const router= Router();
+
+router.get('/',[validateJWT],publicationGet);
+
+router.delete('/:idPublication',[validateJWT],)
+
+router.put('/:idPublication',[validateJWT,
+    verifyIdPublication,
+    check('category').custom(verifyLengthCategory),
+    check('title','The title of publication is required').not().isEmpty(),
+    check('mainText','The main text is required for publication').not().isEmpty(),
+    check('category','Required minimum one the category of publication in form array').isArray().not().isEmpty(),
+    validateFields
+],publicationPut);
+
 router.post('/',[validateJWT,
     check('title','The title of publication is required').not().isEmpty(),
     check('mainText','The main text is required for publication').not().isEmpty(),
