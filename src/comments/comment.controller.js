7f8c8ba24c403c8ce367af35ffcf39c393ca2dab
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-03-03 12:59:58 -0600

    Se agrego commentPost

--- src/comments/comment.controller.js
+++ src/comments/comment.controller.js
@@ -2,10 +2,28 @@ import {request, response} from 'express';
 import Comment from '../comments/comment.model.js';
 import User from '../users/user.model.js';
 
+export const commentPost = async (req=request, res=response)=>{
+    const {id} =req.user;
+    let userLog = await User.findById(id);
+    const {idPublication,comment}=req.body;
+    const newComment = new Comment({comment:comment,idUser:id,idPublication:idPublication});
+    userLog.comments.push(newComment.id);
+    await User.findByIdAndUpdate(id,{comments:userLog.comments});
+    newComment.save();
+    res.status(200).json({
+        newComment,
+    });
+}
+
 export const commentsGet = async (req=request, res=response)=>{
     const {id} =req.user;
     const userLog =await User.findById(id);
+    let myComments=[];
+    for(let idComment of userLog.comments){
+        const comment = await Comment.findById(idComment);
+        myComments.push(comment);
+    }
     res.status(200).json({
-        comments:userLog.comments
+        myComments
     });
 }
--- src/comments/comment.routes.js
+++ src/comments/comment.routes.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import { validateJWT } from "../middlewares/validate-jwt.js";
-import { commentsGet } from "./comment.controller.js";
+import { commentPost,commentsGet } from "./comment.controller.js";
+
+import { validateFields } from "../middlewares/validate-field.js";
+import { existPublication } from "../helpers/db-validator.js";
 
 const router = Router();
 
 router.get('/',[validateJWT],commentsGet);
+
+router.post('/',[
+    validateJWT,
+    check('comment','The comment is required to publicate comment').not().isEmpty(),
+    check('idPublication','A mongo ID is required').isMongoId(),
+    check('idPublication').custom(existPublication),
+    validateFields
+],commentPost);
 
 export default router;
